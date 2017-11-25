81e913edeaf8b146050048a26e14aab2701a0012
Chiro1994 <grzegorz.komorowski.1994@gmail.com>
2017-11-25 18:14:05 +0100

    Dodany konstruktor do klasy Product

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Product.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Product.java
@@ -13,6 +13,15 @@ public class Product {
 	public Product() {
 	}
 
+	public Product(String productId, BigDecimal productPrice, String productName, Date productSnapshotDate,
+			String productType) {
+		this.productId = productId;
+		this.productPrice = productPrice;
+		this.productName = productName;
+		this.productSnapshotDate = productSnapshotDate;
+		this.productType = productType;
+	}
+
 	public String getProductId() {
 		return productId;
 	}
