99ef6af31c14455b7625216d48e993470207f386
smoczynska.justyna <smoczynska.justyna@gmail.com>
2017-12-12 18:56:42 +0100

    Dodanie nowej klasy Product

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Product.java
@@ -0,0 +1,24 @@
+package pl.com.bottega.ecommerce.sales.domain.offer;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * Created by Justyna on 12.12.2017.
+ */
+public class Product {
+
+    private Money price;
+
+    private String productId;
+
+    private BigDecimal productPrice;
+
+    private String productName;
+
+    private Date productSnapshotDate;
+
+    private String productType;
+
+
+}
