6d4a83c3307bf34e8eb35986e533fabda174621f
TotallyUniqueUsername <andrzejstrek@o2.pl>
2018-01-05 21:22:14 +0100

    testowy commit

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Offer.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Offer.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Offer {
-private List<OfferItem> availabeItems = new ArrayList<OfferItem>();
+private List<OfferItem> availabeItems = new ArrayList<OfferItem>(); 
 	
 	private List<OfferItem> unavailableItems = new ArrayList<OfferItem>();
 	
